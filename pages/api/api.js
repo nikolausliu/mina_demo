0621b2b5d1418a740cb04322a683162c395fabaf
nikolausliu <18010855044@163.com>
2018-02-14 11:23:34 +0800

    delete index and logs 2 initial folder and some file mod

--- pages/api/api.js
+++ pages/api/api.js
@@ -1,76 +1,89 @@
 Page({
-
-  /**
-   * 页面的初始数据
-   */
   data: {
-    apilist: []
-  },
-
-  /**
-   * 生命周期函数--监听页面加载
-   */
-  onLoad: function (options) {
-    this.setData({
-      apilist: [
-        {
-          name: 'test1'
-        },
-        {
-          name: 'test2'
-        }
+    list: [{
+      id: 'open-api',
+      name: '开放接口',
+      open: false,
+      pages: [
+        { name: '微信登录', key: 'login' },
+        { name: '获取用户信息', key: 'getUserInfo' },
+        { name: '发起支付', key: 'requestPayment' },
+        { name: '转发', key: 'onShareAppMessage' },
+        { name: '转发按钮', key: 'share-button' },
+        { name: '客服消息', key: 'customMessage' },
+        { name: '模板消息', key: 'templateMessage' }
       ]
-    }, function () {
-      console.log('设置数据成功')
-    })
+    }, {
+      id: 'interface',
+      name: '界面',
+      open: false,
+      pages: [
+        { name: '设置界面标题', key: 'setNavigationBarTitle' },
+        { name: '标题栏加载动画', key: 'navigationBarLoading' },
+        { name: '页面跳转', key: 'navigate' },
+        { name: '下拉刷新', key: 'pullDownRefresh' },
+        { name: '创建动画', key: 'createAnimation' },
+        { name: '创建绘画', key: 'createContext' },
+        { name: '显示操作菜单', key: 'action-sheet' },
+        { name: '显示模态弹窗', key: 'modal' },
+        { name: '显示消息提示框', key: 'toast' },
+      ]
+    }, {
+      id: 'device',
+      name: '设备',
+      open: false,
+      pages: [
+        { name: '获取手机网络状态', key: 'getNetworkType' },
+        { name: '监听手机网络变化', key: 'onNetWorkStatusChange' },
+        { name: '获取手机系统信息', key: 'getSystemInfo' },
+        { name: '监听重力感应数据', key: 'onAccelerometerChange' },
+        { name: '监听罗盘数据', key: 'onCompassChange' },
+        { name: '打电话', key: 'makePhoneCall' },
+        { name: '扫码', key: 'scanCode' }
+      ]
+    }, {
+      id: 'network',
+      name: '网络',
+      open: false,
+      pages: [
+        { name: '发起一个请求', key: 'request' },
+        { name: 'WebSocket', key: 'WeoSocket' },
+        { name: '上传文件', key: 'uploadFile' },
+        { name: '下载文件', key: 'downloadFile' }
+      ]
+    }, {
+      id: 'location',
+      name: '位置',
+      open: false,
+      pages: [
+        { name: '获取当前位置', key: 'getLocation' },
+        { name: '查看位置', key: 'openLocation' },
+        { name: '使用原生地图选择位置', key: 'chooseLocation' }
+      ]
+    },
+    {
+      id: 'data',
+      name: '数据',
+      open: false,
+      pages: [
+        { name: '数据存储', key: 'storage' },
+      ]
+    },]
   },
-
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-    
+  toggle: function (e) {
+    const id = e.currentTarget.id, list = this.data.list
+    for (let i = 0, len = list.length; i < len; i++) {
+      if (id === list[i].id) {
+        list[i].open = !list[i].open;
+      } else {
+        list[i].open = false;
+      }
+    }
+    this.setData({
+      list: list
+    });
   },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
   onShow: function () {
-    
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-    
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-    
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-    
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-    
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-    
+    console.log(`当前页面路由为 ${this.route},页面栈为 `, getCurrentPages())
   }
 })
